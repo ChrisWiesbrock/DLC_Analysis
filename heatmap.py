ab9145ab1b7a574654451538097f41a476a37307
ChrisWiesbrock <christopher.wiesbrock@rwth-aachen.de>
2023-03-01 14:25:33 +0100

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -6,6 +6,8 @@ import os
 #Pfad und Daten werden eingelesen
 path=os.path.abspath(r'C:\Users\wiesbrock\Desktop\2_Cam_Frizi-220630-125958_725_DREADD-220709-134115_Cam1DLC_resnet50_Frizi_MaLabDataDec19shuffle1_450000.csv')
 data=pd.read_csv(path,delimiter=';',header=None)
+#data=pd.read_csv(path,delimiter=';',header=None)
+num_bin=5
 
 #Werte für die Position der Nase werden in Variablen überführt
 snout_x=data[7]
@@ -22,9 +24,9 @@ snout_x=snout_x[4:].astype(float)
 snout_y=snout_y[4:].astype(float)
 
 #Erstellung der Heatmap und Speicherung der normalisierten Position
-h,x_edges,y_edges,mesh=plt.hist2d(snout_x,snout_y,bins=5, cmap='Greys')
+h,x_edges,y_edges,mesh=plt.hist2d(snout_x,snout_y,bins=num_bin, cmap='Greys')
 h=np.transpose(h)
 
 h=h/len(snout_x)
 
-plt.hist2d(snout_x,snout_y,bins=5, cmap='Greys')
+plt.hist2d(snout_x,snout_y,bins=num_bin, cmap='Greys')
